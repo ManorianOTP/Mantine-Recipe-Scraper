44a11bbf414236c65e4e988338905f604bcaed3e
Aaron Scarff <aaron.scarff@bt.com>
2024-12-07 10:31:54 +0000

    editable text stuff

--- components/editable-text/editable-text.tsx
+++ components/editable-text/editable-text.tsx
@@ -1,27 +1,46 @@
 'use client';
 
-import { Textarea } from '@mantine/core';
+import { useState } from 'react';
+import { Edit } from 'lucide-react';
+import { ActionIcon, Group, Stack, Text, Textarea } from '@mantine/core';
 import { useHtmlData } from '@/app/contexts/HtmlDataContext';
 
 export default function EditableText() {
   const { recipeData } = useHtmlData();
+  const [isEditing, setIsEditing] = useState(false);
+  const [currentDescription, setCurrentDescription] = useState(recipeData?.description || '');
+
   if (!recipeData) return null;
 
+  const handleEditClick = () => setIsEditing(true);
+  const handleSave = () => setIsEditing(false); // Add save logic if needed
+
   return (
-    <>
-      <Textarea
-        variant="unstyled"
-        radius="xs"
-        autosize
-        defaultValue={recipeData.description}
-        styles={{
-          input: {
-            fontSize: 'inherit',
-            lineHeight: 'inherit',
-            color: 'inherit',
-          },
-        }}
-      />
-    </>
+    <Stack>
+      {isEditing ? (
+        <Textarea
+          variant="unstyled"
+          radius="xs"
+          autosize
+          value={currentDescription}
+          onChange={(event) => setCurrentDescription(event.target.value)}
+          onBlur={handleSave} // Save changes on blur
+          styles={{
+            input: {
+              fontSize: 'inherit',
+              lineHeight: 'inherit',
+              color: 'inherit',
+            },
+          }}
+        />
+      ) : (
+        <Text>
+          <ActionIcon onClick={handleEditClick} size="sm" radius="xl">
+            <Edit size={16} />
+          </ActionIcon>
+          {currentDescription}
+        </Text>
+      )}
+    </Stack>
   );
 }
