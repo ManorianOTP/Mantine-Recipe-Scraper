4d1194e95790df4898b4d9f70ac6208bdc3c347a
ManorianOTP <43821910+ManorianOTP@users.noreply.github.com>
2024-12-29 16:17:18 +0000

    Working upsert functionality

--- app/api/add-recipe/route.tsx
+++ app/api/add-recipe/route.tsx
@@ -1,41 +1,61 @@
-import { type NextRequest, NextResponse } from 'next/server';
-import { createClient } from '@/utils/supabase/server';
-import { Recipe } from '@/app/types';
+import { type NextRequest, NextResponse } from 'next/server'
+import { createClient } from '@/utils/supabase/server'
+import { Recipe } from '@/app/types'
 
 type RecipeRequestBody = {
-  recipes: Recipe[]; // Adjust based on actual recipe structure
-};
+  recipes: Recipe[]
+}
 
-export async function POST(request: NextRequest) {
+export async function POST (request: NextRequest) {
   try {
-    const supabase = await createClient();
+    const supabase = await createClient()
 
     const {
       data: { user },
-      error: userError,
-    } = await supabase.auth.getUser();
+      error: userError
+    } = await supabase.auth.getUser()
 
     if (userError || !user) {
-      return NextResponse.json({ error: 'User not authenticated' }, { status: 401 });
+      return NextResponse.json(
+        { error: 'User not authenticated' },
+        { status: 401 }
+      )
     }
 
-    const { recipes } = await request.json() as RecipeRequestBody;
+    const { recipes } = (await request.json()) as RecipeRequestBody
 
-    const { error } = await supabase
-      .from('userRecipes')
-      .insert([{ recipes, user_id: user.id }]);
+    const { data, error } = await supabase.rpc('upsert_user_recipe', {
+      p_user_id: user.id,
+      p_recipe: recipes[0]
+    })
 
     if (error) {
-      return NextResponse.json({ error: error.message }, { status: 500 });
+      console.error('Error upserting recipe:', error)
+      // Handle the error as needed
+    } else {
+      console.log('Recipe upserted successfully')
     }
 
-    return NextResponse.json({ message: 'Data inserted successfully' }, { status: 200 });
-    
+    // const { error } = await supabase
+    //   .from('userRecipes')
+    //   .insert([{ recipes, user_id: user.id }]);
+
+    if (error) {
+      return NextResponse.json({ error: error.message }, { status: 500 })
+    }
+
+    return NextResponse.json(
+      { message: 'Data inserted successfully' },
+      { status: 200 }
+    )
   } catch (error) {
-    return NextResponse.json({ error: 'Internal Server Error' }, { status: 500 });
+    return NextResponse.json(
+      { error: 'Internal Server Error' },
+      { status: 500 }
+    )
   }
 }
 
 export const config = {
-  runtime: 'experimental-edge',
-};
+  runtime: 'experimental-edge'
+}
