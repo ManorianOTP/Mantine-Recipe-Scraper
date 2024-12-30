019913d06c89f802465cfedde79eeacab2f2c279
ManorianOTP <43821910+ManorianOTP@users.noreply.github.com>
2024-12-30 16:07:31 +0000

    ssr profile check

--- components/header-bar/header-bar.tsx
+++ components/header-bar/header-bar.tsx
@@ -1,33 +1,53 @@
-'use client';
+import Image from 'next/image'
+import Link from 'next/link'
+import { Group, Text } from '@mantine/core'
+import { ColorSchemeToggle } from '../color-scheme-toggle/color-scheme-toggle'
+import ScrapeBox from '../scrape-box/scrape-box'
+import classes from './header-bar.module.css'
+import { createClient } from '@/utils/supabase/server'
 
-import Image from 'next/image';
-import Link from 'next/link';
-import { Group, Text } from '@mantine/core';
-import { ColorSchemeToggle } from '../color-scheme-toggle/color-scheme-toggle';
-import ScrapeBox from '../scrape-box/scrape-box';
-import classes from './header-bar.module.css';
+export default async function HeaderBar () {
+  const supabase = await createClient()
+  const {
+    data: { user },
+    error: userError
+  } = await supabase.auth.getUser()
 
-export default function HeaderBar() {
   return (
     <header className={classes.header}>
       <div className={classes.inner}>
         <Group>
-          <Image priority src="/recipe-icon.png" alt="FlavorSync Logo" width={50} height={50} />
+          <Image
+            priority
+            src='/recipe-icon.png'
+            alt='FlavorSync Logo'
+            width={50}
+            height={50}
+          />
 
-          <Link className={`${classes.link} ${classes.responsiveLink}`} href="/">
-            <Text size="lg" fw={700} className={classes.responsiveLink}>
+          <Link
+            className={`${classes.link} ${classes.responsiveLink}`}
+            href='/'
+          >
+            <Text size='lg' fw={700} className={classes.responsiveLink}>
               FlavorSync
             </Text>
           </Link>
         </Group>
         <ScrapeBox />
-        <Group gap="xs" wrap="nowrap">
+        <Group gap='xs' wrap='nowrap'>
           <ColorSchemeToggle />
-          <Link className={classes.link} href="/login">
-            Login
-          </Link>
+          {!user ? (
+            <Link className={classes.link} href='/login'>
+              Login
+            </Link>
+          ) : (
+            <Link className={classes.link} href='/private'>
+              Profile
+            </Link>
+          )}
         </Group>
       </div>
     </header>
-  );
+  )
 }
