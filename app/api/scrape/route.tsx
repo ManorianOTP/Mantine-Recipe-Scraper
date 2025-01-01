838a1df69ab0323a2e69d8bf1431c5688f77df82
ManorianOTP <43821910+ManorianOTP@users.noreply.github.com>
2025-01-01 16:29:21 +0000

    Downloads to temp buffer

--- app/api/scrape/route.tsx
+++ app/api/scrape/route.tsx
@@ -1,63 +1,81 @@
-import { NextRequest, NextResponse } from 'next/server';
-import sharp from 'sharp';
-import * as path from 'path';
-import axios from 'axios';
-import * as cheerio from 'cheerio';
-import { Recipe } from '@/app/types';
+import { NextRequest, NextResponse } from 'next/server'
+import sharp from 'sharp'
+import * as path from 'path'
+import axios from 'axios'
+import * as cheerio from 'cheerio'
+import { Recipe } from '@/app/types'
+import { createClient } from '@/utils/supabase/server'
+import { error } from 'console'
 
-export async function GET(req: NextRequest) {
-  const { searchParams } = new URL(req.url);
-  const recipeUrl = searchParams.get('url');
+export async function GET (req: NextRequest) {
+  const { searchParams } = new URL(req.url)
+  const recipeUrl = searchParams.get('url')
+  const supabase = await createClient()
 
   try {
     if (!recipeUrl) {
-      throw new Error('Recipe URL is null');
+      throw new Error('Recipe URL is null')
     }
 
-    const { data } = await axios.get(recipeUrl);
-    const $ = cheerio.load(data);
+    const { data } = await axios.get(recipeUrl)
+    const $ = cheerio.load(data)
 
-    const title = $('h1').text().trim();
+    const title = $('h1').text().trim()
 
-    const ingredients: string[] = [];
+    const ingredients: string[] = []
     $('#ingredients-list .list-item').each((_, element) => {
-      ingredients.push($(element).text().trim());
-    });
+      ingredients.push($(element).text().trim())
+    })
 
-    const method: string[] = [];
+    const method: string[] = []
     $('.js-piano-recipe-method .method-steps__list-item').each((_, element) => {
-      const step = $(element).find('.editor-content p').text().trim();
+      const step = $(element).find('.editor-content p').text().trim()
       if (step) {
-        method.push(step);
+        method.push(step)
       }
-    });
+    })
 
     // Fetch and save image
-    let imageUrl = $('.post-header__image-container img.image__img').attr('src') || '';
+    let imageUrl =
+      $('.post-header__image-container img.image__img').attr('src') || ''
+
+    let webpImageName = ''
 
     if (imageUrl) {
       // Sanitize the image filename to remove query params and add .webp extension
-      const sanitizedUrl = imageUrl.split('?')[0]; // Get the base URL without query parameters
-      const baseName = path.basename(sanitizedUrl, path.extname(sanitizedUrl)); // Get the base name without extension
-      const webpImageName = `${baseName}.webp`;
-      imageUrl = path.join(process.cwd(), 'public', webpImageName);
-  
+      const sanitizedUrl = imageUrl.split('?')[0] // Get the base URL without query parameters
+      const baseName = path.basename(sanitizedUrl, path.extname(sanitizedUrl)) // Get the base name without extension
+      const webpImageName = `${baseName}.webp`
+      const {
+        data: { user },
+        error: userError
+      } = await supabase.auth.getUser()
+
+      if (userError || !user) {
+        return NextResponse.json(
+          { error: 'User not authenticated' },
+          { status: 401 }
+        )
+      }
+
       try {
         // Download the image as a buffer
         const response = await axios({
           url: sanitizedUrl,
           method: 'GET',
-          responseType: 'arraybuffer',
-        });
-  
+          responseType: 'arraybuffer'
+        })
         // Convert the image buffer to WebP and save it
-        await sharp(response.data)
-          .webp()
-          .toFile(imageUrl);
-  
-        console.log(`Image converted to WebP and saved at: ${imageUrl}`);
+        const webpBuffer = await sharp(response.data).webp().toBuffer()
+
+        await supabase.storage
+          .from('temporary-recipe-images')
+          .upload(`${user.id}/${webpImageName}`, webpBuffer, {
+            contentType: 'image/webp'
+          })
+        console.log(`Image converted to WebP and saved at: ${imageUrl}`)
       } catch (error) {
-        console.error('Error downloading or converting image:', error);
+        console.error('Error downloading or converting image:', error)
       }
     }
 
@@ -65,36 +83,40 @@ export async function GET(req: NextRequest) {
       .find('time')
       .text()
       .split(' mins')
-      .filter(Boolean)[0];
+      .filter(Boolean)[0]
 
     const cookTime = $('.recipe__cook-and-prep .list-item')
       .find('time')
       .text()
       .split(' mins')
-      .filter(Boolean)[1];
+      .filter(Boolean)[1]
 
     const servings = $('.recipe__cook-and-prep .list-item')
       .find('.icon-with-text__children')
       .text()
-      .split('Serves')[1];
+      .split('Serves')[1]
 
-    const description = 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Laudantium adipisci dolore quod nostrum sequi? Cupiditate id soluta obcaecati eos error rem consequuntur, asperiores itaque aut explicabo exercitationem quis perferendis suscipit? Lorem ipsum dolor sit amet consectetur adipisicing elit. Laudantium adipisci dolore quod nostrum sequi? Cupiditate id soluta obcaecati eos error rem consequuntur, asperiores itaque aut explicabo exercitationem quis perferendis suscipit?';
+    const description =
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Laudantium adipisci dolore quod nostrum sequi? Cupiditate id soluta obcaecati eos error rem consequuntur, asperiores itaque aut explicabo exercitationem quis perferendis suscipit? Lorem ipsum dolor sit amet consectetur adipisicing elit. Laudantium adipisci dolore quod nostrum sequi? Cupiditate id soluta obcaecati eos error rem consequuntur, asperiores itaque aut explicabo exercitationem quis perferendis suscipit?'
 
     const recipe: Recipe = {
       title,
       ingredients,
       method,
       description,
-      image: `/${path.basename(imageUrl)}`, // Local path to the saved image
+      image: `/temporary-recipe-images/${webpImageName}`, // Local path to the saved image
       prepTime,
       cookTime,
-      servings,
-    };
+      servings
+    }
 
     // Return the scraped data as a JSON response
-    return NextResponse.json(recipe);
+    return NextResponse.json(recipe)
   } catch (error) {
-    console.error('Error fetching or saving recipe:', error);
-    return NextResponse.json({ message: 'Error fetching recipe' }, { status: 500 });
+    console.error('Error fetching or saving recipe:', error)
+    return NextResponse.json(
+      { message: 'Error fetching recipe' },
+      { status: 500 }
+    )
   }
 }
