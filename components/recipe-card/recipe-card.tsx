cb36f98a7c4d5d2ad0c0b079e406b5d8425a1498
ManorianOTP <43821910+ManorianOTP@users.noreply.github.com>
2025-01-08 17:32:20 +0000

    Improved Loading Images

--- components/recipe-card/recipe-card.tsx
+++ components/recipe-card/recipe-card.tsx
@@ -1,59 +1,81 @@
-import { Card, Image, Text, Badge, Button, Group, Rating } from '@mantine/core'
-import { Recipe } from '@/app/types'
-import { useHtmlData } from '@/app/contexts/HtmlDataContext'
+import { Card, Image, Text, Badge, Button, Group, Rating, Skeleton } from '@mantine/core';
+import { Recipe } from '@/app/types';
+import { useHtmlData } from '@/app/contexts/HtmlDataContext';
 import { useRouter } from 'next/navigation';
 import { useEffect, useState } from 'react';
 
-export default function RecipeCard ({ recipe, index }: { recipe: Recipe, index: number}) {
+export default function RecipeCard({ recipe, index }: { recipe: Recipe; index: number }) {
   const { recipeData, setRecipeData, setIndex } = useHtmlData();
   const router = useRouter();
 
-  const [signedUrl, setSignedUrl] = useState<string | null>("-1")
-    useEffect(() => {
-        const fetchSignedUrl = async () => {
-          try {
-            const response = await fetch(
-              `/api/signed-url?src=${encodeURIComponent(recipe.image)}`
-            )
-            const data = await response.json()
-  
-            if (response.ok) {
-              setSignedUrl(data.signedUrl)
-            } else {
-              setSignedUrl(null)
-              console.error('Error fetching signed URL:', data.error)
-            }
-          } catch (error) {
-            setSignedUrl(null)
-            console.error('Error fetching signed URL:', error)
-          }
-        }
-        fetchSignedUrl()
-    }, [])
-  
-    if (signedUrl === "-1") return null
+  const [signedUrl, setSignedUrl] = useState<string | null>(null);
+  const [loading, setLoading] = useState(true);
 
-    
+  useEffect(() => {
+    const fetchSignedUrl = async () => {
+      try {
+        const response = await fetch(`/api/signed-url?src=${encodeURIComponent(recipe.image)}`);
+        const data = await response.json();
+
+        if (response.ok) {
+          setSignedUrl(data.signedUrl);
+        } else {
+          console.error('Error fetching signed URL:', data.error);
+        }
+      } catch (error) {
+        console.error('Error fetching signed URL:', error);
+      } finally {
+        setLoading(false);
+      }
+    };
+    fetchSignedUrl();
+  }, [recipe.image]);
+
   return (
-    <Card shadow='sm' padding='lg' radius='md' withBorder>
+    <Card shadow="sm" padding="lg" radius="md" withBorder>
       <Card.Section>
-        <Image src={signedUrl} height={160} alt={recipe.title + ' image'} fallbackSrc="https://placehold.co/600x400?text=Placeholder"/>
+        {loading ? (
+          <Skeleton height={160} />
+        ) : signedUrl ? (
+          <Image
+            src={signedUrl}
+            height={160}
+            alt={`${recipe.title} image`}
+            fallbackSrc="https://placehold.co/600x400?text=Placeholder"
+          />
+        ) : (
+          <Image
+            src="https://placehold.co/600x400?text=Placeholder"
+            height={160}
+            alt="Placeholder image"
+          />
+        )}
       </Card.Section>
 
-      <Group justify='space-between' mt='md'>
+      <Group justify="space-between" mt="md">
         <Text fw={500}>{recipe.title}</Text>
-        <Badge color='icon'>Tag</Badge>
+        <Badge color="icon">Tag</Badge>
         {/* <Badge color='icon'>{recipe.tag}</Badge> */}
       </Group>
       <Rating fractions={2} />
       {/* {recipe.rating} */}
-      <Text size='sm' c='dimmed' lineClamp={3}>
+      <Text size="sm" c="dimmed" lineClamp={3}>
         {recipe.description}
       </Text>
       {/* Indexes from 1 for some reason */}
-      <Button color='icon' fullWidth mt='md' radius='md' onClick={() => {setRecipeData(recipe); setIndex(index+1); router.push('/recipe');}}>
+      <Button
+        color="icon"
+        fullWidth
+        mt="md"
+        radius="md"
+        onClick={() => {
+          setRecipeData(recipe);
+          setIndex(index + 1);
+          router.push('/recipe');
+        }}
+      >
         More Details
       </Button>
     </Card>
-  )
+  );
 }
--- components/recipe-details/recipe-details.tsx
+++ components/recipe-details/recipe-details.tsx
@@ -1,6 +1,6 @@
-'use client'
+'use client';
 
-import { Clock, Save, Users } from 'lucide-react'
+import { Clock, Save, Users } from 'lucide-react';
 import {
   Button,
   Image,
@@ -9,19 +9,20 @@ import {
   Flex,
   Group,
   Rating,
+  Skeleton,
   Stack,
   Text,
-  Title
-} from '@mantine/core'
-import { useHtmlData } from '@/app/contexts/HtmlDataContext'
-import EditableText from '@/components/editable-text/editable-text'
-import { useEffect, useState } from 'react'
+  Title,
+} from '@mantine/core';
+import { useHtmlData } from '@/app/contexts/HtmlDataContext';
+import EditableText from '@/components/editable-text/editable-text';
+import { useEffect, useState } from 'react';
 
-export default function RecipeDetails () {
-  const { recipeData, index, setIndex } = useHtmlData()
-  if (!recipeData) return null
-  const [signedUrl, setSignedUrl] = useState<string | null>("-1")
-  const [recipes, setRecipes] = useState([])
+export default function RecipeDetails() {
+  const { recipeData, index, setIndex } = useHtmlData();
+  const [signedUrl, setSignedUrl] = useState<string | null>(null);
+  const [loading, setLoading] = useState(true);
+  const [recipes, setRecipes] = useState([]);
 
   useEffect(() => {
     const fetchData = async () => {
@@ -29,52 +30,55 @@ export default function RecipeDetails () {
         const response = await fetch('/api/get-recipes', {
           method: 'GET',
           headers: {
-            'Content-Type': 'application/json'
-          }
-        })
+            'Content-Type': 'application/json',
+          },
+        });
 
         if (!response.ok) {
-          const errorData = await response.json()
-          console.error('Error:', errorData.error)
-          return
+          const errorData = await response.json();
+          console.error('Error:', errorData.error);
+          return;
         }
 
-        const data = await response.json()
-        setRecipes(data[0].recipes || [])
-        setSignedUrl(null)
-        console.log('Success:', data.message)
+        const data = await response.json();
+        setRecipes(data[0].recipes || []);
+        console.log('Success:', data.message);
       } catch (error) {
-        setSignedUrl(null)
-        console.error('Request failed:', error)
+        console.error('Request failed:', error);
       }
-    }
+    };
 
-    fetchData()
-  }, [])
+    fetchData();
+  }, []);
+
   useEffect(() => {
-    if (recipeData.image) {
+    if (recipeData?.image) {
       const fetchSignedUrl = async () => {
         try {
           const response = await fetch(
             `/api/signed-url?src=${encodeURIComponent(recipeData.image)}`
-          )
-          const data = await response.json()
+          );
+          const data = await response.json();
 
           if (response.ok) {
-            setSignedUrl(data.signedUrl)
+            setSignedUrl(data.signedUrl);
           } else {
-            console.error('Error fetching signed URL:', data.error)
+            console.error('Error fetching signed URL:', data.error);
           }
         } catch (error) {
-          console.error('Error fetching signed URL:', error)
+          console.error('Error fetching signed URL:', error);
+        } finally {
+          setLoading(false);
         }
-      }
+      };
 
-      fetchSignedUrl()
+      fetchSignedUrl();
+    } else {
+      setLoading(false);
     }
-  }, [recipeData.image])
+  }, [recipeData?.image]);
 
-  if (signedUrl === "-1") return null
+  if (!recipeData) return null;
 
   return (
     <>
@@ -82,26 +86,27 @@ export default function RecipeDetails () {
         align={{ base: 'center', sm: 'start' }}
         direction={{ base: 'column', sm: 'row' }}
       >
-        <Image
-          width={350}
-          height={350}
-          src={signedUrl}
-          alt='Recipe Image'
-          style={{ objectFit: 'cover' }}
-          fallbackSrc="https://placehold.co/600x400?text=Placeholder"
-        />
-        <Stack
-          justify='space-between'
-          style={{ flexGrow: '1', paddingLeft: '2px' }}
-        >
+        {loading ? (
+          <Skeleton width={350} height={350} />
+        ) : (
+          <Image
+            width={350}
+            height={350}
+            src={signedUrl || 'https://placehold.co/600x400?text=Placeholder'}
+            alt="Recipe Image"
+            style={{ objectFit: 'cover' }}
+          />
+        )}
+
+        <Stack justify="space-between" style={{ flexGrow: '1', paddingLeft: '2px' }}>
           <div>
-            <Group justify='space-between' align='flex-start'>
+            <Group justify="space-between" align="flex-start">
               <Title order={1} flex={1} mb={2}>
-                <EditableText dataKey='title' />
+                <EditableText dataKey="title" />
               </Title>
               <Button
                 leftSection={<Save />}
-                color='icon'
+                color="icon"
                 maw={101}
                 mr={5}
                 mt={10}
@@ -109,33 +114,36 @@ export default function RecipeDetails () {
                   let recipe = [JSON.parse(JSON.stringify(recipeData))];
 
                   try {
-                    let newRecipe = true
+                    let newRecipe = true;
                     if (index !== null) {
-                      newRecipe = false
-                      recipe[0].image= recipe[0].image.replace('/temporary-recipe-images', '/recipe-images')
+                      newRecipe = false;
+                      recipe[0].image = recipe[0].image.replace(
+                        '/temporary-recipe-images',
+                        '/recipe-images'
+                      );
                     }
-                    console.log(recipe)
-                    console.log(index)
+                    console.log(recipe);
+                    console.log(index);
                     const response = await fetch('/api/add-recipe', {
                       method: 'POST',
                       headers: {
-                        'Content-Type': 'application/json'
+                        'Content-Type': 'application/json',
                       },
-                      body: JSON.stringify({ recipe, index })
-                    })
+                      body: JSON.stringify({ recipe, index }),
+                    });
 
                     if (!response.ok) {
-                      const errorData = await response.json()
-                      console.error('Error:', errorData.error)
-                      return
+                      const errorData = await response.json();
+                      console.error('Error:', errorData.error);
+                      return;
                     }
-                    const data = await response.json()
+                    const data = await response.json();
                     if (newRecipe) {
-                      setIndex(recipes.length + 1)
+                      setIndex(recipes.length + 1);
                     }
-                    console.log('Success:', data.message)
+                    console.log('Success:', data.message);
                   } catch (error) {
-                    console.error('Request failed:', error)
+                    console.error('Request failed:', error);
                   }
                 }}
               >
@@ -144,47 +152,47 @@ export default function RecipeDetails () {
             </Group>
             <Rating fractions={2} style={{ marginBottom: '2px' }} />
             <Text>Category</Text>
-            <EditableText dataKey='description' />
+            <EditableText dataKey="description" />
           </div>
           <div>
             <Group preventGrowOverflow style={{ whiteSpace: 'pre' }}>
               <div style={{ display: 'flex', alignItems: 'center' }}>
                 <Clock size={18} />
-                <Text ml='xs'>Prep Time: </Text>
-                <EditableText dataKey='prepTime' />
+                <Text ml="xs">Prep Time: </Text>
+                <EditableText dataKey="prepTime" />
               </div>
               <div style={{ display: 'flex', alignItems: 'center' }}>
                 <Clock size={18} />
-                <Text ml='xs'>Cook Time: </Text>
-                <EditableText dataKey='cookTime' />
+                <Text ml="xs">Cook Time: </Text>
+                <EditableText dataKey="cookTime" />
               </div>
 
               <div style={{ display: 'flex', alignItems: 'center' }}>
                 <Users size={18} />
-                <Text ml='xs'>Serves: </Text>
-                <EditableText dataKey='servings' />
+                <Text ml="xs">Serves: </Text>
+                <EditableText dataKey="servings" />
               </div>
             </Group>
           </div>
         </Stack>
       </Flex>
-      <Divider my='sm' />
+      <Divider my="sm" />
 
       <section>
         <Title order={2}>Ingredients</Title>
         <div style={{ marginTop: '0.5rem' }}>
           {recipeData.ingredients.map((_ingredient, index) => (
             <div key={index}>
-              <Group gap='xs'>
-                <Checkbox color='icon' style={{ padding: '2px' }} />
-                <EditableText dataKey='ingredients' index={index} />
+              <Group gap="xs">
+                <Checkbox color="icon" style={{ padding: '2px' }} />
+                <EditableText dataKey="ingredients" index={index} />
               </Group>
             </div>
           ))}
         </div>
       </section>
 
-      <Divider my='sm' />
+      <Divider my="sm" />
 
       <section>
         <Title order={2}>Method</Title>
@@ -192,11 +200,11 @@ export default function RecipeDetails () {
           {recipeData.method.map((_ingredient, index) => (
             <div key={index} style={{ marginBottom: '1rem' }}>
               <Text>Step {index + 1}</Text>
-              <EditableText dataKey='method' index={index} />
+              <EditableText dataKey="method" index={index} />
             </div>
           ))}
         </div>
       </section>
     </>
-  )
+  );
 }
